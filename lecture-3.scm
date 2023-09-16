ee17dbb3b9d19fa18411fac3bee7413240cf7931
teixeirazeus <teixeira.zeus@gmail.com>
2023-09-16 12:59:57 -0300

    l3b

--- lecture-3.scm
+++ lecture-3.scm
@@ -94,13 +94,56 @@
         (beside p (right-push p (- n 1) a)
         a)))
 
+; construção de sistemas robustos
+; um pequena variação no problema, 
+; deve causar uma pequena variação na solução
+; resolva problemas pequenos, resolvendo uma classe de problemas
+; criando uma abstração para você se expressar
 
+(define (deriv exp var)
+    (cond ((constant? exp var) 0)
+        ((same-var? exp var) 1)
+        ((sum? exp) (make-sum (deriv (addend exp) var)
+                                (deriv (augend exp) var)))
+        ((product? exp) (make-sum
+                            (make-product (multiplier exp)
+                                            (deriv (multiplicand exp) var))
+                            (make-product (deriv (multiplier exp) var)
+                                            (multiplicand exp))))
+        (else (error "unknown expression type -- DERIV" exp))))
 
+(define (constant? exp var)
+    (and (atom? exp) (not (eq? exp var))))
 
+(define (same-var? exp var)
+    (and (atom? exp) (eq? exp var)))
 
+(define (sum? exp)
+    (and (not (atom? exp))
+    (eq (car exp) '+)))
 
+(define (make-sum a1 a2)
+    (list '+ a1 a2))
 
+(define a1 cadr)
+(define a2 caddr)
 
+(define (product? exp)
+    (and (not (atom? exp))
+    (eq? (car exp) '*)))
 
+(define (makeproduct m1 m2)
+    (list '* m1 m2))
 
+(define m1 cadr)
+(define m2 caddr)
 
+(define (make-sum a1 a2)
+    (cond ((and (number? a1)
+            (number? a2))
+            (+ a1 a2))
+            ((and (number? a1) (= a1 0))
+            a2 
+            ((and (number? a2) (= a2 0))
+            a1)
+            (else (list '+ a1 a2)))))
